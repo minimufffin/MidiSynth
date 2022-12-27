29b0e5d0f5033d00076502c570ee6429fb4337d2
Matthew <mprovince123@hotmail.com>
2022-12-26 17:25:25 -0800

    Parse out unwanted status bytes, initial read for note on and off

--- prog/prog.ino
+++ prog/prog.ino
@@ -1,22 +1,61 @@
 #define BAUDRATE 9600   // change to midi baud rate
 
-int incomingByte;       // byte sent from midi controller
+// status messages (without midi channel)
+#define NOTEON 0x90
+#define NOTEOFF 0x80
+
+int incomingByte = 0;       // byte sent from midi controller
+int runningStatus = 0;      // last midi status byte
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(BAUDRATE);
 }
 
+void printByte(int byte)
+{
+  if (byte > 0)
+  {
+    Serial.println("Recieved: 0x");
+    Serial.println(incomingByte, HEX);
+  }
+}
+
+void readMessage(int firstByte)
+{
+  int noteValue;
+  int noteVelocity;
+  if (runningStatus & 0xF0 == NOTEON || runningStatus & 0xF0 == NOTEOFF)
+  {
+    delay(1);   // make sure that the full midi message goes through
+    if (firstByte == runningStatus)
+      noteValue = Serial.read();
+    else
+      noteValue = firstByte;
+    noteVelocity = Serial.read();
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  if (Serial.available() > 0)
+
+  // First, read a byte for next message
+  incomingByte = Serial.read();
+  printByte(incomingByte);
+  if (incomingByte > 0 && incomingByte & 0x80)
+    runningStatus = incomingByte; // update status byte
+
+  // Next, make sure status byte is either note on or note off
+  while (incomingByte != -1 && (runningStatus & 0xF0) != NOTEON && (runningStatus & 0xF0) != NOTEOFF)
   {
-    // read incoming byte
     incomingByte = Serial.read();
-
-    Serial.print("Recieved: 0x");
-    Serial.println(incomingByte, HEX);
-
+    printByte(incomingByte);
+    if (incomingByte > 0 && incomingByte & 0x80)
+      runningStatus = incomingByte;
   }
+
+  // at this point if byte is not -1, we can read a full message
+  if (incomingByte > 0)
+    readMessage(incomingByte);
+
 }
